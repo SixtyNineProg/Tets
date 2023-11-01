c9a5fcac2a918664000a0e803b6fdd7a33de95df
Dmitry Klimov <dima.klimov.98@inbox.ru>
2023-11-01 22:40:55 +0300

    tests education 1.0 [*] init test

---
+++ src/test/java/ru/clevertec/product/mapper/impl/ProductMapperImplTest.java
@@ -0,0 +1,19 @@
+package ru.clevertec.product.mapper.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ProductMapperImplTest {
+
+    @Test
+    void toProduct() {
+      }
+
+    @Test
+    void toInfoProductDto() {
+      }
+
+    @Test
+    void merge() {
+      }
+}
