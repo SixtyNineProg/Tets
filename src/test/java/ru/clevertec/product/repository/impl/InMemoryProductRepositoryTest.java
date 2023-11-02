b139222cf4cc01632856abb5531e244429c66937
Dmitry Klimov <dima.klimov.98@inbox.ru>
2023-11-02 19:28:52 +0300

    tests education 1.0 [*] init test

---
+++ src/test/java/ru/clevertec/product/repository/impl/InMemoryProductRepositoryTest.java
@@ -0,0 +1,23 @@
+package ru.clevertec.product.repository.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class InMemoryProductRepositoryTest {
+
+    @Test
+    void findById() {
+      }
+
+    @Test
+    void findAll() {
+      }
+
+    @Test
+    void save() {
+      }
+
+    @Test
+    void delete() {
+      }
+}
