539ab2b951728c07320972aa5439931bfc3c9bf9
Dmitry Klimov <dima.klimov.98@inbox.ru>
2023-11-02 19:29:18 +0300

    tests education 1.0 [*] refactor

--- src/main/java/ru/clevertec/product/data/ProductDto.java
+++ src/main/java/ru/clevertec/product/data/ProductDto.java
@@ -4,20 +4,11 @@ import java.math.BigDecimal;
 
 public record ProductDto(
 
-        /**
-         * {@link ru.clevertec.product.entity.Product}
-         */
-        String name,
+    /** {@link ru.clevertec.product.entity.Product} */
+    String name,
 
+    /** {@link ru.clevertec.product.entity.Product} */
+    String description,
 
-        /**
-         * {@link ru.clevertec.product.entity.Product}
-         */
-        String description,
-
-
-        /**
-         * {@link ru.clevertec.product.entity.Product}
-         */
-        BigDecimal price) {
-}
+    /** {@link ru.clevertec.product.entity.Product} */
+    BigDecimal price) {}
